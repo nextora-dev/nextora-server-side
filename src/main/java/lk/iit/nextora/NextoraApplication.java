dc7802037a361b91f713f29e85d12ed0b9837c75
Haritha0705 <harithawikramasinha2003@gmail.com>
2026-01-01 22:21:36 +0530

    Initial commit

---
+++ src/main/java/lk/iit/nextora/NextoraApplication.java
@@ -0,0 +1,13 @@
+package lk.iit.nextora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/iit/nextora/NextoraApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.iit.nextora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
