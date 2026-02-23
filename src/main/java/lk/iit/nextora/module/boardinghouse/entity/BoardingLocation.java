b96379a04f520c6c3ea3dc43ae3fe16fc713fd91
isuru38 <isururavishan0627@gmail.com>
2026-02-23 10:53:02 +0530

    Task-1012.17: update BoardingHouse entity structure and relationships

--- src/main/java/lk/iit/nextora/module/boardinghouse/entity/BoardingLocation.java
+++ src/main/java/lk/iit/nextora/module/boardinghouse/entity/BoardingLocation.java
@@ -4,24 +4,24 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "boarding_locations")
+@Table(name = "boarding_location")
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class BoardingLocation {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 100)
     private String city;
 
-    @Column(nullable = false, length = 255)
     private String address;
 
     private Double latitude;
+
     private Double longitude;
+
 }
