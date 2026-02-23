059fed70b08bd737c711a77b1b2342fcfbded6f0
isuru38 <isururavishan0627@gmail.com>
2026-02-23 10:51:51 +0530

    Task-1012.15: update BoardingContact table name and remove column annotations

--- src/main/java/lk/iit/nextora/module/boardinghouse/entity/BoardingContact.java
+++ src/main/java/lk/iit/nextora/module/boardinghouse/entity/BoardingContact.java
@@ -4,21 +4,20 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "boarding_contacts")
+@Table(name = "boarding_contact")
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class BoardingContact {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 15)
     private String contactNumber1;
 
-    @Column(length = 15)
     private String contactNumber2;
+
 }
