9d80452c7d98c0d12d4a311ec0b97024cddbb125
isuru38 <isururavishan0627@gmail.com>
2026-02-28 00:57:53 +0530

    Task-1012.26: update BoardingHouseRepository to remove paging/filter methods and add findByIdAndIsDeletedFalse

--- src/main/java/lk/iit/nextora/module/boardinghouse/repository/BoardingHouseRepository.java
+++ src/main/java/lk/iit/nextora/module/boardinghouse/repository/BoardingHouseRepository.java
@@ -1,24 +1,13 @@
 package lk.iit.nextora.module.boardinghouse.repository;
 
 import lk.iit.nextora.module.boardinghouse.entity.BoardingHouse;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface BoardingHouseRepository extends JpaRepository<BoardingHouse, Long> {
 
-    Page<BoardingHouse> findByIsDeletedFalse(Pageable pageable);
-
-    Page<BoardingHouse> findByLocation_CityIgnoreCaseAndIsDeletedFalse(String city, Pageable pageable);
-
-    Page<BoardingHouse> findByMonthlyRentBetweenAndIsDeletedFalse(Double min, Double max, Pageable pageable);
-
-    Page<BoardingHouse> findByLocation_CityIgnoreCaseAndMonthlyRentBetweenAndIsDeletedFalse(
-            String city, Double min, Double max, Pageable pageable);
-
-    Page<BoardingHouse> findByGenderTypeAndIsDeletedFalse(
-            lk.iit.nextora.module.boardinghouse.entity.BoardingGenderType genderType, Pageable pageable);
-
-    Page<BoardingHouse> findByWithFoodAndWithFurnitureAndIsDeletedFalse(
-            Boolean withFood, Boolean withFurniture, Pageable pageable);
+    Optional<BoardingHouse> findByIdAndIsDeletedFalse(Long id);
 }
