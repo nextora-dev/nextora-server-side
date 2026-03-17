7c4c6cc1df9d4f716197a147240f052455a8e8b5
janindu_wije <janinduwije21@gmail.com>
2026-03-17 13:35:03 +0530

    Task-1007.32:add SruCategoryRepository for managing SruCategory entities in the intranet module

--- src/main/java/lk/iit/nextora/module/intranet/repository/SruCategoryRepository.java
+++ src/main/java/lk/iit/nextora/module/intranet/repository/SruCategoryRepository.java
@@ -5,18 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SruCategoryRepository extends JpaRepository<SruCategory, Long> {
-
-    List<SruCategory> findAllByIsDeletedFalseAndIsActiveTrueOrderByCategoryNameAsc();
-
-    Optional<SruCategory> findByCategorySlugAndIsDeletedFalse(String categorySlug);
-
-    @Query("SELECT s FROM SruCategory s LEFT JOIN FETCH s.videos WHERE s.categorySlug = ?1 AND s.isDeleted = false")
-    Optional<SruCategory> findByCategorySlugWithVideos(String categorySlug);
 
     @Query("SELECT s FROM SruCategory s WHERE s.categorySlug = '_root' AND s.isDeleted = false")
     Optional<SruCategory> findRootCategory();
